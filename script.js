c90e8d8addeb18f27d2b9c79cdc85d67f892e495
MTScg <42665652+MTScg@users.noreply.github.com>
2018-08-24 15:32:10 +0200

    V5

--- script.js
+++ script.js
@@ -37,3 +37,17 @@ document.querySelector('#berechnen').addEventListener('click', berechne_BMI);
 				
 	document.querySelector("output").textContent = bmi;
   }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
